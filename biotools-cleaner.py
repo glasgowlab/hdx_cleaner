aab7db777372b4dcf530cdb4fb3ba1f2e9e67bad
Malcolm Wells <mw3079@columbia.edu>
2023-09-07 17:03:08 -0400

    add rangeslist behavior and fix colorbar

--- biotools-cleaner.py
+++ biotools-cleaner.py
@@ -57,6 +57,7 @@ parser.add_argument('--t', '--table', dest='table', help="paths to input csvs", 
 parser.add_argument('--o', '--out', dest='out', help="path to output csv")
 parser.add_argument('--p', '--plots', dest='plots', help="directory for output plots")
 parser.add_argument('--ov', '--overlap', dest='overlap', help="how to treat overlapping duplicates: keep/drop/select?")
+parser.add_argument('--r', '--rangeslist', dest='rangeslist', help="destination for rangeslist")
 parser.add_argument('--RTC', dest='RTC', type=float, help='RT cutoff for duplicates')
 parser.add_argument('--MZC', dest='MZC', type=float, help='M/Z cutoff for duplicates')
 parser.add_argument('--scoreC', dest='scoreC', type=float, help='Score threshold for initial curve fit')
@@ -96,6 +97,20 @@ print('mean score: ' + str(np.mean(data['Score'])))
 ymax=len(data)
 xmax = max(data['Score'])
 
+# TEMP scatterplot of full dataset
+
+def floathelper(n):
+    if n == '-':
+        return 0
+    return float(n.strip())
+figure, (ax1) = plt.subplots(1, 1, figsize=(5,5))
+data['MSMS Cov.(%)'] = data['MSMS Cov.(%)'].apply(floathelper)
+plt.yscale("log")
+plt.scatter(data['MSMS Cov.(%)'], data['Score'].apply(float)+1)
+plt.savefig(plots + '/scatter-score-coverage.png')
+
+
+
 
 # PLOTTING SECTION
 
@@ -112,7 +127,7 @@ plt.savefig(plots + '/hist-scores-all.png')
 # scatterplot of full dataset
 figure, (ax1) = plt.subplots(1, 1, figsize=(5,5))
 
-plt.scatter(data['Meas. M/z'].apply(float), data['Dev.(ppm)'].apply(float), c=data['Score'], norm=colors.LogNorm())
+plt.scatter(data['Meas. M/z'].apply(float), data['Dev.(ppm)'].apply(float), c=data['Score'], norm=colors.LogNorm(vmax=xmax))
 plt.colorbar(label='Score')
 plt.xlabel('M/z')
 plt.ylabel('deviation (ppm)')
@@ -140,7 +155,7 @@ figure, (ax1) = plt.subplots(1, 1, figsize=(5,5))
 popt, pcov = opt.curve_fit(invfn, hsdata['Meas. M/z'].apply(float), hsdata['Dev.(ppm)'].apply(float), maxfev=maxfev,bounds=([-np.inf, -np.inf, -np.inf], [np.inf, min(data['Meas. M/z'].apply(float))-100, np.inf]))
 plt.plot(np.sort(data['Meas. M/z'].apply(float)), invfn(np.sort(data['Meas. M/z'].apply(float)), *popt))
 
-plt.scatter(hsdata['Meas. M/z'].apply(float), hsdata['Dev.(ppm)'].apply(float), c=hsdata['Score'], norm=colors.LogNorm())
+plt.scatter(hsdata['Meas. M/z'].apply(float), hsdata['Dev.(ppm)'].apply(float), c=hsdata['Score'], norm=colors.LogNorm(vmax=xmax))
 plt.colorbar(label='Score')
 plt.xlabel('M/z')
 plt.ylabel('deviation (ppm)')
@@ -167,7 +182,7 @@ figure, (ax1) = plt.subplots(1, 1, figsize=(5,5))
 
 plt.plot(np.sort(data['Meas. M/z'].apply(float)), invfn(np.sort(data['Meas. M/z'].apply(float)), *popt))
 
-plt.scatter(cleaned['Meas. M/z'].apply(float), cleaned['Dev.(ppm)'].apply(float), c=cleaned['Score'], norm=colors.LogNorm())
+plt.scatter(cleaned['Meas. M/z'].apply(float), cleaned['Dev.(ppm)'].apply(float), c=cleaned['Score'], norm=colors.LogNorm(vmax=xmax))
 plt.colorbar(label='Score')
 plt.xlabel('M/z')
 plt.ylabel('deviation (ppm)')
@@ -256,7 +271,7 @@ if not flagged.empty:
     # scatterplot of flagged data
     figure, (ax1) = plt.subplots(1, 1, figsize=(5,5))
     plt.plot(np.sort(data['Meas. M/z'].apply(float)), invfn(np.sort(data['Meas. M/z'].apply(float)), *popt))
-    plt.scatter(flagged['Meas. M/z'].apply(float), flagged['Dev.(ppm)'].apply(float), c=flagged['Score'], norm=colors.LogNorm(vmin=1, vmax=max(10,max(flagged['Score']))))
+    plt.scatter(flagged['Meas. M/z'].apply(float), flagged['Dev.(ppm)'].apply(float), c=flagged['Score'], norm=colors.LogNorm(vmin=1, vmax=xmax))
     plt.colorbar(label='Score')
     plt.xlabel('M/z')
     plt.ylabel('deviation (ppm)')
@@ -285,7 +300,7 @@ if not yellow.empty:
     # scatterplot of yellow flag data
     figure, (ax1) = plt.subplots(1, 1, figsize=(5,5))
     plt.plot(np.sort(data['Meas. M/z'].apply(float)), invfn(np.sort(data['Meas. M/z'].apply(float)), *popt))
-    plt.scatter(yellow['Meas. M/z'].apply(float), yellow['Dev.(ppm)'].apply(float), c=yellow['Score'], norm=colors.LogNorm(vmin=1, vmax=max(10,max(flagged['Score']))))
+    plt.scatter(yellow['Meas. M/z'].apply(float), yellow['Dev.(ppm)'].apply(float), c=yellow['Score'], norm=colors.LogNorm(vmin=1, vmax=xmax))
     plt.colorbar(label='Score')
     plt.xlabel('M/z')
     plt.ylabel('deviation (ppm)')
@@ -309,6 +324,8 @@ clean = topscores.drop(flagged.index.union(yellow.index))
 
 clean.sort_values(['Start', 'End'], inplace=True)
 clean.to_csv(outpath, index=False)
+if args.rangeslist:
+    clean[['Start', 'End']].to_csv(args.rangeslist, index=False)
 print('clean: ' + str(clean.shape))
 print('mean score: ' + str(np.mean(clean['Score'])))
 
@@ -316,7 +333,7 @@ print('mean score: ' + str(np.mean(clean['Score'])))
 figure, (ax1) = plt.subplots(1, 1, figsize=(5,5))
 plt.plot(np.sort(data['Meas. M/z'].apply(float)), invfn(np.sort(data['Meas. M/z'].apply(float)), *popt))
 
-plt.scatter(clean['Meas. M/z'].apply(float), clean['Dev.(ppm)'].apply(float), c=clean['Score'], norm=colors.LogNorm())
+plt.scatter(clean['Meas. M/z'].apply(float), clean['Dev.(ppm)'].apply(float), c=clean['Score'], norm=colors.LogNorm(vmax=xmax))
 plt.colorbar(label='Score')
 plt.xlabel('M/z')
 plt.ylabel('deviation (ppm)')
